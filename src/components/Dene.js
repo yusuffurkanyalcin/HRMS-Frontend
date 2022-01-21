b7a4a4d59bfd6423a2f7218335384374b9e385fc
Yusuf Furkan Yalçın <55233993+ekmekarasibuzdolabi@users.noreply.github.com>
2022-01-21 21:55:14 +0300

    refactoring

---
+++ src/components/Dene.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import styles from "../styles/dene.module.css";
+export default function Dene() {
+  return (
+    <div>
+      <Button className={styles.buton}></Button>
+    </div>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+import {
+  Button,
+  Container,
+  Nav,
+  Dropdown,
+  DropdownButton,
+} from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import styles from "../styles/navbar.module.css";
+export default function Navbar() {
+  return (
+    <Container fluid className={styles.container}>
+      <Nav className={styles.nav} activeKey="/#">
+        <Nav.Item className="me-auto p-2 bd-highlight">
+          <Button variant="otline-light" className={styles.hrmsLogo}>
+            HRMS.net
+          </Button>
+          <Button variant="otline-light" className={`${styles.leftButton} ${styles.searchJob}`}>
+            İş Ara
+            <FontAwesomeIcon icon={faSearch} className={styles.searchIcon}/>
+          </Button>
+
+          <Button variant="otline-light" className={styles.leftButton}>
+            Üniversite Rehberi
+          </Button>
+          <Button variant="otline-light" className={styles.leftButton}>
+            Pozisyon Rehberi
+          </Button>
+        </Nav.Item>
+        <Nav.Item className={styles.leftNavItem}>
+          <Button
+            className="px-4 navButton "
+            variant="outline-danger"
+            size="md"
+          >
+            ÜYE OL
+          </Button>
+        </Nav.Item>
+        <Nav.Item className={styles.leftNavItem}>
+          <Button className=" px-3 " variant="outline-danger" size="md">
+            ÜYE GİRİŞİ
+          </Button>
+        </Nav.Item>
+        <Nav.Item className={styles.leftNavItem}>
+          <DropdownButton title="İşveren" align="end" size="md">
+            <Dropdown.Item href="#/action1">Giriş Yap</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item href="#/action2">Kayıt Ol</Dropdown.Item>
+          </DropdownButton>
+        </Nav.Item>
+      </Nav>
+    </Container>
+  );
+}
