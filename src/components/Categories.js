6c3ef3cf29078a3428aa5c7fa35490dcf81c4e90
Yusuf Furkan Yalçın <55233993+ekmekarasibuzdolabi@users.noreply.github.com>
2022-01-25 12:21:04 +0300

    category cards are processing

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,23 +1,66 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-import Card from "react-bootstrap/Card";
+import React, { useEffect } from "react";
+import managerImage from "../images/manager.jpg";
+import styles from "../styles/category.module.css"
 export default function Categories() {
+  useEffect(() => {
+    console.log(managerImage);
+  }, []);
+
   return (
-    <Container>
-      <Card style={{ width: "18rem" }}>
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            Card Subtitle
-          </Card.Subtitle>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Card.Link href="#">Card Link</Card.Link>
-          <Card.Link href="#">Another Link</Card.Link>
-        </Card.Body>
-      </Card>
-    </Container>
+    <div className="container">
+      <div style={{maxWidth:"540px"}} className={styles.firtCard}>
+        <div>aeaweqwe</div>
+      </div>
+      <br/>
+      <div className="card mb-3 " style={{maxWidth:"540px"}}>
+        <div className="row g-0">
+          <div className="col-md-7">
+            <img
+              src={managerImage}
+              className="img-fluid rounded-start"
+              alt="Manager picture"
+            />
+          </div>
+          <div className="col-md-5">
+            <div className="card-body">
+              <h5 className="card-title">Card Title</h5>
+              <p className="card-text">This is manager card</p>
+              <p className="card-text">
+                <small className="text-muted">Last updated ..</small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+     
+
+      <div
+        className="card  text-dark "
+        style={{ maxWidth: "540px" }}
+      >
+        <img
+          src={managerImage}
+          className="card-img img-fluid rounded-start"
+          alt="..."
+        />
+        <div className="card-img-overlay">
+          <h5 className="card-title">
+            <strong>CARD TITLE</strong>{" "}
+          </h5>
+          <p className="card-text">
+            <strong>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </strong>
+          </p>
+          <p className="card-text">Last updated 3 mins ago</p>
+        </div>
+      </div>
+    </div>
   );
 }
+
+/*
+
+*/
