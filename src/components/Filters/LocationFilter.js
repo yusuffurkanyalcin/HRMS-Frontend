1276cb5f92d959400968b2dce58bb606a2fa520f
Yusuf Furkan Yalçın <55233993+ekmekarasibuzdolabi@users.noreply.github.com>
2022-01-29 18:46:09 +0300

    filters refactoring

--- src/components/Filters/LocationFilter.js
+++ src/components/Filters/LocationFilter.js
@@ -1,11 +1,69 @@
 import React from "react";
 import Accordion from "react-bootstrap/Accordion";
+import { Formik, Field, Form } from "formik";
+import styles from "../../styles/locationFilter.module.css";
+
 export default function LocationFilter() {
   return (
-    <Accordion defaultActiveKey={["0"]} alwaysOpen style={{minWidth:"188px"}}>
-      <Accordion.Item eventKey="0" >
+    <Accordion
+      defaultActiveKey={["0"]}
+      alwaysOpen
+      style={{ minWidth: "188px" }}
+    >
+      <Accordion.Item eventKey="0">
         <Accordion.Header>Şehir</Accordion.Header>
-        <Accordion.Body></Accordion.Body>
+        <Accordion.Body>
+          <Formik
+            initialValues={{
+              checked: [],
+            }}
+            onSubmit={(values) => {
+              alert(JSON.stringify(values, null, 2));
+            }}
+          >
+            {({ values }) => (
+              <Form>
+                <div
+                  role="group"
+                  aria-labelledby="checkbox-group"
+                  className={styles.chechboxGroupDiv}
+                >
+                  <div>
+                    <label>
+                      <Field type="checkbox" name="checked" value="Ankara" />
+                      Ankara
+                    </label>
+                  </div>
+                  <div>
+                    <label>
+                      <Field type="checkbox" name="checked" value="İstanbul" />
+                      İstanbul
+                    </label>
+                  </div>
+                  <div>
+                    <label>
+                      <Field type="checkbox" name="checked" value="İzmir" />
+                      İzmir
+                    </label>
+                  </div>
+                  <div>
+                    <label>
+                      <Field type="checkbox" name="checked" value="Mersin" />
+                      Mersin
+                    </label>
+                  </div>
+                  <div>
+                    <label>
+                      <Field type="checkbox" name="checked" value="Kars" />
+                      Kars
+                    </label>
+                  </div>
+                </div>
+                <button type="submit">Submit</button>
+              </Form>
+            )}
+          </Formik>
+        </Accordion.Body>
       </Accordion.Item>
     </Accordion>
   );
